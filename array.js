bb4c5319cd9b4527b585088723b49c2102e3b001
Silvia <131654979+Silrg@users.noreply.github.com>
2024-02-21 19:38:19 +0100

    Add files via upload

---
+++ array.js
@@ -0,0 +1,32 @@
+let especies = [
+  {
+    nombre: "Sonic",
+    especie: "Erizo",
+    peso: "28",
+    imagen:
+      "https://w7.pngwing.com/pngs/831/795/png-transparent-movie-sonic-thumbnail.png",
+  },
+  {
+    nombre: "Tails",
+    especie: "Zorro",
+    peso: "20",
+    imagen:
+      "https://e7.pngegg.com/pngimages/142/819/png-clipart-tails-sonic-the-hedgehog-sonic-generations-sonic-3d-rock-mammal-carnivoran.png",
+  },
+  {
+    nombre: "Knuckles",
+    especie: "Equidna",
+    peso: "40",
+    imagen:
+      "https://www.pngmart.com/files/13/Knuckles-The-Echidna-Transparent-Background.png",
+  },
+  {
+    nombre: "Amy Rose",
+    especie: "Erizo",
+    peso: "20",
+    imagen:
+      "https://www.pngkey.com/png/detail/149-1491375_amy-rose-by-fentonxd-on-deviantart-so-pretty.png",
+  },
+];
+
+module.exports = especies;
